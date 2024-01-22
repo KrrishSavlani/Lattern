386069b7dafcb9667e10ff589dadad2b17a4f0d2
krrish savlani <savlanikrrish@gmail.comgit config --global user.email savlanikrrish@gmail.com>
2024-01-22 12:02:29 +0530

    lettern s added

---
+++ S.lettern.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+void printSpaces(int count)  
+{  for (int i = 0; i< count ; i++ )
+    {printf(" ");}
+}
+  
+void printAsterisks(int count) 
+{ for (int i = 0; i< count ; i++ )
+    {printf("*");}
+}
+
+void printpatternS(int size)
+
+{
+    for (int i = 0 ; i< size ;i++)
+    {
+        if(i < 1){printAsterisks(size);}
+        printf("\n");
+        if(i < size-5)
+        {printAsterisks(1);
+        }
+         
+        if(i< size -5){printf("\n");
+        printAsterisks(size);}
+        
+         if(i< size - 5){printf("\n");
+         printSpaces(size-1);
+        printAsterisks(1);}
+        
+        if(i< size -5){printf("\n");
+        printAsterisks(size);}
+    }
+}
+
+
+int main() {
+int size = 6;
+printpatternS(size);
+    return 0;
+}
